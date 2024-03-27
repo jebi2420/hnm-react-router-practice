9e8fce3c150a4e25271df8bcafe475d099028d03
jebi2420 <jebi2420@gmail.com>
2024-03-27 18:42:13 +0900

    [add] page 폴더및 파일들 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetail from './page/ProductDetail';
 
 // 1. 전체상품페이지, 로그인, 상품상세페이지
+// 1-1. navigation 바
 // 2. 전체 상품페이지에서는 전체 상품을 볼 수 있다.
 // 3. 로그인 버튼을 누르면 로그인 페이지가 나온다.
 // 4. 상품디테일을 눌렀으나, 로그인이 안 되어있을 경우, 로그인 페이지가 먼저 나오낟.
@@ -14,8 +18,9 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={</>} />
-        <Route path="/" element={</>} />
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
     </div>
   );
