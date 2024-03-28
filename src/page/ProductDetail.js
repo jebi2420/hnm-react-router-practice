598b7383b2f7ebe2e9e54c00419fc04a71cbe190
jebi2420 <jebi2420@gmail.com>
2024-03-28 10:47:35 +0900

    [add] size 드롭다운 항목, col 수정

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -5,10 +5,10 @@ const ProductDetail = () => {
   return (
     <Container>
         <Row>
-        <Col lg={6}>
+        <Col lg={6} sm={12}>
           <img className='detail-img' alt='' src='https://noona-hnm.netlify.app/ankle-jeans.jpeg' />
         </Col>
-        <Col lg={6}>
+        <Col lg={6} sm={12}>
           <div className="detail-cap">
             <div className='detail-title'>슬림핏 맘 하이웨이스트 앵클 진</div>
             <div className='detail-price'>₩29900</div>
@@ -18,9 +18,9 @@ const ProductDetail = () => {
                 사이즈 선택
               </Dropdown.Toggle>
               <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                <Dropdown.Item href="#/action-1">S</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">M</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">L</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
             <Button variant="dark" className='add-btn'>추가</Button>
