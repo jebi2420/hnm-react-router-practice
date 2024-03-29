2c2df0ed814f145ea2e46990d6f49b798bd91be2
jebi2420 <jebi2420@gmail.com>
2024-03-29 22:46:03 +0900

    [add] 상품 검색하기 2

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import ProductCard from '../component/ProductCard';
+import { useSearchParams } from 'react-router-dom';
 
 const ProductAll = () => {
     // UI그려주는건 useState
     const [productList, setProductList] = useState([]);
+    const [query, setQuery]= useSearchParams();
 
     const getProducts = async() => {
+        let searchQuery = query.get('q') || "";
+        console.log("searchQ : ",searchQuery)
         // json-server의 Endpoints
-        let url = `https://my-json-server.typicode.com/jebi2420/hnm-react-router-practice/products`
+        let url = `https://my-json-server.typicode.com/jebi2420/hnm-react-router-practice/products?q=${searchQuery}`
         let response = await fetch(url);
         let data = await response.json();
         setProductList(data);
     }
 
     useEffect(()=>{
-        getProducts()
-    },[])
+        getProducts() // 처음 만들어졌을때만 호출 됨
+    },[query])// query가 바뀌면 다시 getProduct() 호출해줘
 
     console.log("productList:",productList)
 
