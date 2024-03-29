b8eed6bf7c9254d169d69df949f0447b6dc7cbf3
jebi2420 <jebi2420@gmail.com>
2024-03-29 13:59:15 +0900

    [fix] url 변경

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
     <div>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<ProductAll />} />
+        <Route path="/products" element={<ProductAll />} />
         <Route path="/login" element={<Login  setAuthenticate={setAuthenticate}/>} />
         <Route path="/product/:id" element={<PrivateRoute authenticate={authenticate} />} />
       </Routes>
