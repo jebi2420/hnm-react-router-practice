59f77fcecd1b71d9d07e1b1ab900feb907e72479
jebi2420 <jebi2420@gmail.com>
2024-04-02 19:37:56 +0900

    [add] redux devtool 적용

--- src/page/Login.js
+++ src/page/Login.js
@@ -23,7 +23,7 @@ const Login = ({setAuthenticate}) => {
       <Form onSubmit={(event) => loginUser(event)}> 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" onChange={(event)=> setId(event.target.valueAsDate)}/>
+        <Form.Control type="email" placeholder="Enter email" onChange={(event)=> setId(event.target.value)}/>
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -31,7 +31,7 @@ const Login = ({setAuthenticate}) => {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" onChange={(event)=> setPassword(event.target.valueAsDate)}/>
+        <Form.Control type="password" placeholder="Password" onChange={(event)=> setPassword(event.target.value)}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import  {thunk} from 'redux-thunk';
 import rootReducer from "./reducers" // index는 기본파일이라 끝까지 입력안해도 인식함
 
-let store = createStore(rootReducer, applyMiddleware(thunk))
+let store = createStore(rootReducer, 
+    composeWithDevTools(applyMiddleware(thunk)))
 
 export default store;
